3911710c08bc1700d3aca43f2aa6f32e334de321
DanielaMontes <66233799+DanielaMontes@users.noreply.github.com>
2022-05-26 00:40:52 -0500

    Update conn.php

--- conn.php
+++ conn.php
@@ -11,7 +11,7 @@ $conn = mysqli_init();
 //mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
 // Establish the connection
-mysqli_real_connect($conn, 'https://web-app-cloud.azurewebsites.net', '10.1.0.6@vic', 'vic', 'mv1', NULL, NULL);
+mysqli_real_connect($conn, 'https://web-app-cloud.azurewebsites.net', '10.1.0.6@vic', 'vic', 'mv1',3306, NULL, NULL);
 
 //If connection failed, show the error
 if (mysqli_connect_errno())
