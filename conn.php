8c19f3bde5eea2ea2fecd86135fa6ca7f0fbfe14
DanielaMontes <66233799+DanielaMontes@users.noreply.github.com>
2022-05-26 01:04:00 -0500

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 
 $host = '10.1.0.6';
-$username = 'vic';
+$username = '10.1.0.6@vic';
 $password = 'vic';
 $db_name = 'mv1';
 
@@ -11,7 +11,7 @@ $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
 // Establish the connection
-mysqli_real_connect($conn, '10.1.0.6', 'vic', 'vic', 'mv1',3306, NULL, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, '10.1.0.6', '10.1.0.6@vic', 'vic', 'mv1',3306, NULL, MYSQLI_CLIENT_SSL);
 
 //If connection failed, show the error
 if (mysqli_connect_errno())
